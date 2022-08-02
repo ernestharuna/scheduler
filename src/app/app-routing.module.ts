4b7179b8f0e366ca711cdf90c22e3115ebcc2e49
Ernest Haruna <ernestharuna1@gmail.com>
2022-08-02 11:45:49 +0100

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', pathMatch: 'full', component: HomePageComponent },
+{ path: 'home', component: HomePageComponent },
+{ path: 'login-page', component: LoginPageComponent },
+{ path: 'admin-layout', component: AdminLayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ScheduleTableComponent } from './schedule-table/schedule-table.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    AdminLayoutComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ScheduleTableComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
