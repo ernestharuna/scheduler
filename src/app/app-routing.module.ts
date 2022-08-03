778a70ac85c8616cff730028242b27d6fdc8ac75
Ernest Haruna <ernestharuna1@gmail.com>
2022-08-03 14:27:31 +0100

    edited and added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,33 +8,19 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
-{ 
-  path: '', pathMatch: 'full', component: HomePageComponent 
-},
-{
-   path: 'home', component: HomePageComponent 
-},
-{
-   path: 'login-page', component: LoginPageComponent
-},
-{
-   path: 'admin-layout', component: AdminLayoutComponent,
-   children: [
-      {
-         path: 'dashboard',
-         component: DashboardComponent
-      },
-      {
-         path: 'users-profile',
-         component: UsersProfileComponent
-      }
-   ]
-},
-
+   { path: '', pathMatch: 'full', component: HomePageComponent },
+   { path: 'home', component: HomePageComponent },
+   { path: 'login-page', component: LoginPageComponent },
+   { path: 'admin-layout', component: AdminLayoutComponent,
+      children: [
+         { path: 'dashboard', component: DashboardComponent },
+         { path: 'users-profile', component: UsersProfileComponent }
+      ]
+   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+   imports: [RouterModule.forRoot(routes)],
+   exports: [RouterModule]
 })
 export class AppRoutingModule { }
